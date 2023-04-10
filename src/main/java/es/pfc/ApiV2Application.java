cf0b3ddab68badd27e74174852246dd97fe7b7e0
dguttman <dguttmandevelop@gmail.com>
2023-04-10 10:47:39 +0200

    Initial commit

---
+++ src/main/java/es/pfc/ApiV2Application.java
@@ -0,0 +1,13 @@
+package es.pfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiV2Application.class, args);
+    }
+
+}
